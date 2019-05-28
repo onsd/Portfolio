449f2772e814c6e11d353b6dd30bcd50fc1ddd10
Takamichi Omori <merrytakamerry@gmail.com>
2019-05-29 00:03:54 +0900

    ちょっとふやした

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,106 +1,98 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/rbhatia46",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
-    "education":[
-      {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+  "imagebaseurl": "https://onsd.github.io/",
+  "name": "Takamichi Omori",
+  "role": "Web, Infrastructure, Security Developer",
+  "linkedinId": "Your LinkedIn Id",
+  "skypeid": "Your skypeid",
+  "roleDescription": "Web開発、仮想化・クラウドなどのインフラ、セキュリティが好きです",
+  "socialLinks": [
+    {
+      "name": "github",
+      "url": "http://github.com/onsd",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "Twitter",
+      "url": "http://twitter.com/onsd_",
+      "className": "fa fa-twitter"
+    }
+  ],
+  "aboutme": "豊田工業高等専門学校の5年生です。一年間ロシア連邦に留学していました。",
+  "address": "Japan",
+  "website": "https://onsd.github.io",
+  "education": [
+    {
+      "UniversityName": "National Insititute Technology, Toyota Collage",
+      "specialization": "Department of Information and Computer Engineering",
+      "MonthOfPassing": "May",
+      "YearOfPassing": "2020-2014",
+      "Achievements": "在学中"
+    },
+    {
+      "UniversityName": "Гимназии № 15",
+      "specialization": "",
+      "MonthOfPassing": "Aug",
+      "YearOfPassing": "2015",
+      "Achievements": "Москва,Клин. 留学先のホストスクールです"
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "Some Company",
+      "specialization": "Some specialization",
+      "MonthOfLeaving": "Jan",
+      "YearOfLeaving": "2018",
+      "Achievements": "Some Achievements"
+    },
+
+  ],
+  "skillsDescription": "Your skills here",
+  "skills": [
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "Golang"
+    },
+    {
+      "skillname": "Docker"
+    },
+    {
+      "skillname": "CI/CD"
+    },
+  ],
+  "portfolio": [
+    {
+      "name": "project1",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    },
+    {
+      "name": "project2",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project.jpg"
+    },
+    {
+      "name": "project3",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project2.png"
+    },
+    {
+      "name": "project4",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
